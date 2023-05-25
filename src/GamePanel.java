9c8880370e3df64a21a30df2b1fe414a6a5c647a
Luca Esposito <luca.esposito.98@outlook.it>
2023-05-25 14:55:47 +0200

    move the snake in all direction and collision

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -90,11 +90,33 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	
 	public void checkApple() {
-		
+		if((x[0] == appleX) && (y[0] == appleY)) {
+			bodyParts++;
+		}
 	}
 	
 	public void checkCollisions() {
+		for(int i=bodyParts;i>0;i--) {
+			if((x[0] == x[i]) && (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		if(x[0]<0) {
+			running = false;
+		}
+		if(x[0]>SCREEN_WIDTH) {
+			running = false;
+		}
+		if(y[0]<0) {
+			running = false;
+		}
+		if(y[0]>SCREEN_HEIGHT) {
+			running = false;
+		}
 		
+		if(!running) {
+			timer.stop();
+		}
 	}
 	
 	public void gameOver(Graphics g) {
@@ -114,7 +136,28 @@ public class GamePanel extends JPanel implements ActionListener{
 	public class MyKeyAdapter extends KeyAdapter {
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if(direction!= 'R') {
+						direction = 'L';
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(direction!= 'L') {
+						direction = 'R';
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(direction!= 'D') {
+						direction = 'U';
+					}
+					break;
+				case KeyEvent.VK_DOWN:
+					if(direction!= 'U') {
+						direction = 'D';
+					}
+					break;
+			}
 		}
 	}
 
